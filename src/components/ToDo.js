fde3f2f8e3fa9214b1d73bab0c596a7457a16548
DG Reyes <dxtrreyes15@gmail.com>
2021-08-08 11:01:32 +0800

    initial commit

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,15 +5,18 @@ export default function ToDo({ todoItem, markComplete }) {
     const markTask = () => {
         markComplete(todoItem.id);
     }
+
     return (
         <>
-            <div className="todo-item" style={todoItem.completed ? {textDecoration: "line-through", color: "rgba(250,250,250,.8)"} : {}}>
-                <label >
-                    <input 
+            <div className="todo-item-container" style={todoItem.completed ? {textDecoration: "line-through", color: "rgba(250,250,250,.8)"} : {}}>
+                <label className="todo-item">
+                    <input style={{display: "inline-block"}}
                     type="checkbox" 
-                    onChange={markTask}/>
-                    {'  '}
+                    onChange={markTask}
+                    checked={todoItem.completed ? todoItem.completed : false} />
+                    {' '}
                     {todoItem.title}
+
                 </label>
             </div>
         </>
